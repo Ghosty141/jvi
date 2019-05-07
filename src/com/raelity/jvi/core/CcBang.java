6be20e2f70c1d9806e671d31a2bb4ab2603792a9
ghosty <ghosty141@gmail.com>
2019-05-07 22:55:40 +0200

    Replaced getModifiers() with getModifiersEx()

--- src/com/raelity/jvi/core/CcBang.java
+++ src/com/raelity/jvi/core/CcBang.java
@@ -336,7 +336,7 @@ public static class BangAction extends AbstractColonAction
                 public void keyTyped(KeyEvent e) {
                     e.consume();
                     if(e.getKeyChar() == (KeyEvent.VK_C & 0x1f)
-                        && (e.getModifiers() & KeyEvent.CTRL_DOWN_MASK) != 0) {
+                        && (e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0) {
                         finishBangCommand(false);
                     } else {
                         Util.beep_flush();
